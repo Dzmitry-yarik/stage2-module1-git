61b1d3477fe9a75f44d2f9d1af2ecb56e5021db2
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-04-08 15:48:23 +0300

    Update Repo.java

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,7 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/username/reposotoryName";
+    public static String REPO_LINK = "https://github.com/Dzmitry-yarik/git_my";
+    
+
 }
