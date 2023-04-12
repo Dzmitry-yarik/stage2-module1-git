d700c58ebc2f2c65597b1be10c9641239cac9e73
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-04-12 21:37:46 +0300

    Update Repo.java

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,7 +1,6 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/Dzmitry-yarik/git_my";
-    
-
+    public static String REPO_LINK = "https://github.com/Dzmitry-yarik/MyMJC.git";
+   
 }
